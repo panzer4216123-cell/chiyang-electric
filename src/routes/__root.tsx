f1795a317cf0c215df1d0fa157dc797debe09075
panzer4216123-cell <panzer4216123@gmail.com>
2026-09-06 11:58:36 +0000

    Mobile: no top-right control; system fonts; bottom dock

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -24,19 +24,6 @@ export const Route = createRootRoute({
       { rel: "stylesheet", href: appCss },
       { rel: "manifest", href: "/__grok/manifest.webmanifest" },
       { rel: "apple-touch-icon", href: "/__grok/icon-180.png" },
-      {
-        rel: "preconnect",
-        href: "https://fonts.googleapis.com",
-      },
-      {
-        rel: "preconnect",
-        href: "https://fonts.gstatic.com",
-        crossOrigin: "anonymous",
-      },
-      {
-        rel: "stylesheet",
-        href: "https://fonts.googleapis.com/css2?family=Chiron+Hei+HK:wght@400;500;600&family=Chiron+Sung+HK:wght@500;600;700&display=swap",
-      },
     ],
   }),
   component: RootDocument,
